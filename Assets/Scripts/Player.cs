655c1669e827fd25f105e0d4231622907a532a8c
Zippsson <62258314+Zippsson@users.noreply.github.com>
2021-04-13 14:39:47 +0200

    UI Health and Pickup

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int current_Health;
+    public int max_Health = 100;
+    public int current_Attack = 1;
+    public int pickup_amount = 0;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        current_Health = max_Health;
+        healthBar.SetMaxHealth(max_Health);
+    }
+
+    void Update()
+    {
+          
+    }
+    //TEMP: Just to test the healthbar function.
+    void TakeDamage(int damage)
+    {
+        current_Health -= damage;
+
+        healthBar.SetHealth(current_Health);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "pickup")
+        {
+            Destroy(collision.gameObject);
+            pickup_amount++;
+        }
+    }
+}
