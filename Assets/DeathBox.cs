67db8630868db1de3ec5a0cd1ceafee34db27950
KarateRai <raimon420@gmail.com>
2021-05-18 13:01:20 +0200

    Update DeathBox.cs

--- Assets/DeathBox.cs
+++ Assets/DeathBox.cs
@@ -3,6 +3,21 @@ using UnityEngine;
 public class DeathBox : MonoBehaviour
 {
     [SerializeField] GameObject checkPoint;
+<<<<<<< Updated upstream
+=======
+    [SerializeField] UnityEvent onDeath;
+    private bool takeDmg = true;
+    private void Update()
+    {
+        InvokeRepeating("Invulnerable", 0.1f, 0.2f);
+    }
+
+    void Invulnerable()
+    {
+        takeDmg = true;
+    }
+
+>>>>>>> Stashed changes
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.transform.position = checkPoint.transform.position;
