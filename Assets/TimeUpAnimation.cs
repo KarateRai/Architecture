73b379d4f15f1ac565afe65f3274bb2cd9d1e99f
Algelius <62651753+Algelius@users.noreply.github.com>
2021-05-18 14:15:05 +0200

    Portal effects

--- Assets/TimeUpAnimation.cs
+++ Assets/TimeUpAnimation.cs
@@ -19,7 +19,7 @@ public class TimeUpAnimation : StateMachineBehaviour
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.gameObject.GetComponent<AnimationState>().AnimationEnded(stateInfo);
+        //animator.gameObject.GetComponent<AnimationState>().AnimationEnded(stateInfo);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
